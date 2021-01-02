e28df037fea4b07b8d7e73d3ca7681aad652fb43
Rajdattpawar <rajpawar02@gmail.com>
2021-01-02 18:42:41 +0530

    Update AppTest.java

--- java-app/src/test/java/com/mycompany/app/AppTest.java
+++ java-app/src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World Pipeline" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World from Pipeline again" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World From Pipeline!\"");
+            fail("\"message\" is not \"Hello World from Pipeline again!\"");
         }
     }
 
